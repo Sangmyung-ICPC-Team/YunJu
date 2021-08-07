63c4b19f5ff09fb187aba441cb53d8d5f1756df9
YunJu <bibi02180@naver.com>
2021-08-07 16:52:15 +0900

    baekjoon

--- Baekjoon/15652.py
+++ Baekjoon/15652.py
@@ -1,21 +1,14 @@
 N, M = map(int, input().split())
-visited = [0 for _ in range(N)]
 dfs_list = []
 
 def dfs(count):
-    if count == M:
+    if len(dfs_list) == M:
         print(*dfs_list)
         return
     
-    for i in range(N):
-        if visited[i] == 0:
-            dfs_list.append(i+1)
-
-            dfs(count+1)
-            visited[i] = 1
-            dfs_list.pop()
-
-            for j in range(i+1, N):
-                visited[j] = 0
-
-dfs(0)
+    for i in range(count, N+1):
+        dfs_list.append(i)
+        dfs(i)
+        dfs_list.pop()
+    
+dfs(1)
