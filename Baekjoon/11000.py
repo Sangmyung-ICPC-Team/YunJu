b0a42775b0e3fff153c18fc7abc00f55aac8b3c6
YunJu <bibi02180@naver.com>
2022-01-21 17:12:10 +0900

    baekjoon

---
+++ Baekjoon/11000.py
@@ -0,0 +1,19 @@
+import heapq
+N = int(input())
+qlist = []
+classroom = []
+
+for i in range(N):
+    s, t = map(int, input().split())
+    qlist.append([s, t])
+
+qlist.sort()
+heapq.heappush(classroom, qlist[0][1])
+
+for i in range(1, N):
+    if qlist[i][0] < classroom[0]:
+        heapq.heappush(classroom, qlist[i][1])
+    else:
+        heapq.heappop(classroom)
+        heapq.heappush(classroom, qlist[i][1])
+print(len(classroom))
