be5c1de54a2de3ced2fd2978bcbf41e587ac324a
YunJu <bibi02180@naver.com>
2022-05-25 13:19:06 +0900

    baekjoon

---
+++ Baekjoon/2493.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input()) 
+
+tap = list(map(int,input().split()))
+
+stack = []
+ans = []
+
+for i in range(n):
+    while stack:
+        if stack[-1][1] >= tap[i]: 
+            ans.append(stack[-1][0] + 1)
+            break
+        
+        else: stack.pop()
+
+    if not stack:
+        ans.append(0) 
+    
+    stack.append((i, tap[i]))
+
+for i in ans:
+    print(i, end = ' ')
