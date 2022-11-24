525155dadb9ee34c2163dcf8416f708b1eb9fbda
YunJu <bibi02180@naver.com>
2022-11-24 16:10:06 +0900

    baekjoon

---
+++ Baekjoon/14267.py
@@ -0,0 +1,28 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+n, m = map(int, sys.stdin.readline().rstrip().split())
+
+node = [[] for i in range(n+1)] 
+clist = [0 for i in range(n+1)]
+
+people = list(map(int, sys.stdin.readline().rstrip().split()))
+
+def dfs(num, size):
+    for i in node[num]:
+        if(num < i):
+            clist[i] += size
+            dfs(i, size)
+
+for i in range(1, n+1) :
+    node[i].append(people[i-1])
+    if(people[i-1] != -1): 
+        node[people[i-1]].append(i)
+
+for j in range(m): 
+    i, w = map(int, sys.stdin.readline().rstrip().split())
+    clist[i] += w 
+    dfs(i, w)
+  
+for i in clist[1::]:
+    print(i, end=" ")
